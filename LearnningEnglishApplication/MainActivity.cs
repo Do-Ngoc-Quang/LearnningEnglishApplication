27541e329a0db4b7afc428462abf88e039684eb4
Ngoc Quang Do <info.dongocquang@gmail.com>
2023-12-12 17:06:41 +0700

    Add project files.

---
+++ LearnningEnglishApplication/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Content;
+
+namespace LearnningEnglishApplication
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btn_start;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btn_start = FindViewById<Button>(Resource.Id.btn_start);
+
+            btn_start.Click += Btn_start_Click;
+        }
+
+        private void Btn_start_Click(object sender, System.EventArgs e)
+        {
+            Intent it = new Intent(this, typeof(login)); // Khởi tạo Intent
+            StartActivity(it); // Mở Activity mới
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ LearnningEnglishApplication/login.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnningEnglishApplication
+{
+    [Activity(Label = "login")]
+    public class login : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.login);
+        }
+    }
+}
